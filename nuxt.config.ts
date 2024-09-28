fad18b35b7e81578a87064edbad71be892f2e7dc
kadeem-lewis <email@kadeemlewis.com>
2024-09-28 19:01:49 -0400

    turned off ssr and removed firebase server packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
+  ssr: false,
   devtools: { enabled: true },
   modules: [
     "@nuxt/devtools",
@@ -15,12 +16,20 @@ export default defineNuxtConfig({
   ],
   vuefire: {
     config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
+      apiKey: "AIzaSyAM-lsu1lvfaeh-QBEBJXdEvMAmSuSBVLQ",
+
+      authDomain: "aux-wars.firebaseapp.com",
+
+      projectId: "aux-wars",
+
+      storageBucket: "aux-wars.appspot.com",
+
+      messagingSenderId: "470027436249",
+
+      appId: "1:470027436249:web:0558aea621107f98457ec8",
+    },
+    auth: {
+      enabled: true,
     },
   },
   postcss: {
