206628645684bc30f12997e6808f0ffc707ca2ec
kadeem-lewis <email@kadeemlewis.com>
2024-09-28 13:49:42 -0400

    added firebase env variables to vuefire config and added future version flag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
   modules: [
     "@nuxt/devtools",
@@ -10,6 +13,16 @@ export default defineNuxtConfig({
     "@nuxt/ui",
     "nuxt-vuefire",
   ],
+  vuefire: {
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+    },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
