a405c2c941023b51a47948e95a0e4444a167f743
kadeem-lewis <email@kadeemlewis.com>
2024-09-28 14:45:00 -0400

    create a simple default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>hi</div>
+  <UContainer class="flex flex-col gap-2">
+    <NavBar />
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <p>&copy; 2021</p>
+    </footer>
+  </UContainer>
 </template>
 
 <script setup lang="ts"></script>
