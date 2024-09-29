d27644661a1f2a59425136f72a651cf41150c634
kadeem-lewis <email@kadeemlewis.com>
2024-09-29 01:48:45 -0400

    updated nuxt config to use env for app config variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,17 +21,12 @@ export default defineNuxtConfig({
   ],
   vuefire: {
     config: {
-      apiKey: "AIzaSyAM-lsu1lvfaeh-QBEBJXdEvMAmSuSBVLQ",
-
-      authDomain: "aux-wars.firebaseapp.com",
-
-      projectId: "aux-wars",
-
-      storageBucket: "aux-wars.appspot.com",
-
-      messagingSenderId: "470027436249",
-
-      appId: "1:470027436249:web:0558aea621107f98457ec8",
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
     },
     auth: {
       enabled: true,
