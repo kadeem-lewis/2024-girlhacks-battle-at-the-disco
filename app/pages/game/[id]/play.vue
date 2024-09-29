2caae7f039922c6f5e515e6487f088201aa9d9ee
kadeem-lewis <email@kadeemlewis.com>
2024-09-29 01:53:53 -0400

    continued working on main game area

--- app/pages/game/[id]/play.vue
+++ app/pages/game/[id]/play.vue
@@ -1,20 +1,36 @@
 <template>
-  <div>Your artist is{{ artist.name }}</div>
+  <div v-if="playerData?.role === 'player'">
+    Your artist is{{ artist.name }} Your opponent's artist is
+    {{ opponentData?.artist.name }}
+  </div>
+  <div v-else>
+    <div v-for="player of players" :key="player.id">
+      {{ player.artist.name }}
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { doc, updateDoc } from "firebase/firestore";
+import { collection, doc, updateDoc } from "firebase/firestore";
 
 const db = useFirestore();
 const route = useRoute("game-id-play");
 const user = useCurrentUser();
 
+const artist = await $fetch("/api/random_artist");
+await updateDoc(doc(db, "rooms", route.params.id, "players", user.value!.uid), {
+  artist: artist,
+});
+
+const players = useCollection(
+  collection(db, "rooms", route.params.id, "players"),
+);
+
 const playerData = useDocument(
   doc(db, "rooms", route.params.id, "players", user.value!.uid),
 );
 
-const artist = await $fetch("/api/random_artist");
-await updateDoc(doc(db, "rooms", route.params.id, "players", user.value!.uid), {
-  artist: artist,
+const opponentData = computed(() => {
+  return players.value?.find((player) => player.id !== user.value?.uid);
 });
 </script>
