aafe780d7e8a2ed59a81f4068b098228f24ee5d3
kadeem-lewis <email@kadeemlewis.com>
2024-09-28 22:15:46 -0400

    created route to handle create or join game

---
+++ app/pages/mode.vue
@@ -0,0 +1,67 @@
+<template>
+  <UCard>
+    <template #header>
+      <h2 class="text-center font-bold uppercase">Join or Create a Room</h2>
+    </template>
+    <UForm class="flex flex-col gap-2" :state="state" @submit="handleJoinRoom">
+      <UFormGroup label="Room Code:">
+        <UInput v-model="state.roomCode" />
+      </UFormGroup>
+      <UButton type="submit" block @click="action = 'join'">Join</UButton>
+    </UForm>
+    <UDivider label="or" />
+    <UButton type="submit" block @click="handleCreateRoom">Create</UButton>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+import {
+  arrayUnion,
+  doc,
+  setDoc,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { customAlphabet } from "nanoid";
+
+const db = useFirestore();
+
+const action = ref<"create" | "join" | null>(null);
+
+const user = useCurrentUser();
+
+const state = reactive({
+  roomCode: "",
+});
+
+async function handleCreateRoom() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const nanoid = customAlphabet(characters, 5);
+  const roomId = nanoid();
+  try {
+    await setDoc(doc(db, "rooms", roomId), {
+      createdAt: Timestamp.now(),
+      players: [user.value?.uid],
+      host: user.value?.uid,
+    });
+    navigateTo({
+      path: `/game/${roomId}/lobby`,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function handleJoinRoom() {
+  try {
+    await updateDoc(doc(db, "rooms", state.roomCode), {
+      players: arrayUnion(user.value?.uid),
+    });
+    navigateTo({
+      path: `/game/${state.roomCode}/lobby`,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+</script>
