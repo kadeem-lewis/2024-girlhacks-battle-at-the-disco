f165fd420e46dfd6f6cae964742d0a10984dd6ae
kadeem-lewis <email@kadeemlewis.com>
2024-09-28 21:15:23 -0400

    added logout button to navbar

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,7 +1,12 @@
 <template>
   <header>
     <h1>App Name</h1>
+    <UButton v-if="user" @click="signOut(auth!)">Logout</UButton>
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { signOut } from "firebase/auth";
+const auth = useFirebaseAuth();
+const user = useCurrentUser();
+</script>
